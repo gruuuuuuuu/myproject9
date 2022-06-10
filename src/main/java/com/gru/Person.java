4357fe113f3e53d299f16f692c1420eed74bbfd3
gruuuuuuuu <gru20021104@gmail.com>
2022-06-10 15:15:52 +0800

    initial commit

--- src/main/java/com/gru/Person.java
+++ src/main/java/com/gru/Person.java
@@ -2,8 +2,7 @@ package com.gru;
 
 public class Person {
     public void greeting(){
-        //System.out.println("Hello！");
-        Person p = new Person();
-        p.greeting();
+        System.out.println("Hello！");
+
     }
 }
